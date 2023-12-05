6de85ce42b559ce4710d0a92deb85cb0bcf9c1f1
Yoann Rodière <yoann@hibernate.org>
2023-12-05 11:10:53 +0100

    Reproducer

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 # Reproducer
 
-How to test with the built-in version of Hibernate ORM:
+How to reproduce:
 
 ```shell
-./mvnw clean verify
-# OR, native mode:
 ./mvnw clean verify -Dnative
-```
-
-How to test a newer version of Hibernate ORM:
-
-```shell
-./mvnw clean verify  -Dversion.org.hibernate.orm=6.2.6-SNAPSHOT
-# OR, native mode:
-./mvnw clean package -Dnative -Dversion.org.hibernate.orm=6.2.6-SNAPSHOT
 ```
--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,27 @@
 package org.acme;
 
+import io.smallrye.config.ConfigMapping;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigMapping(prefix = "hello")
+    public interface MyConfig {
+        String message();
+    }
+
+    @Inject
+    MyConfig config;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return config.message();
     }
 }
